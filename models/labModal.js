479541df54d19519e0b11a037649932f95d10555
sameer <a1989@A1989s-MacBook-Pro.local>
2022-07-03 20:44:34 +0500

    lab added

--- models/labModal.js
+++ models/labModal.js
@@ -1,22 +1,29 @@
 const mongoose = require("mongoose");
 
 const LabSchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        softwares: {
-            type: Array,
-            "default": [],
-            required: true,
-        }
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    softwares: {
+      type: Array,
+      default: [],
+      required: true,
+    },
+    qrId: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
-
 
 const Lab = mongoose.model("Lab", LabSchema);
 
